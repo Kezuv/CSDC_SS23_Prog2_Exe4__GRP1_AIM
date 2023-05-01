75f218e530e1d5b28de098ba61c00dbdc9164dc1
Kezuv <armin.foucek@gmail.com>
2023-05-01 19:51:46 +0200

    workding on fxml files

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/controller/MainViewController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/controller/MainViewController.java
@@ -1,18 +1,43 @@
 package at.ac.fhcampuswien.fhmdb.ui.controller;
 
+import at.ac.fhcampuswien.fhmdb.FhmdbApplication;
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.SubScene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
 
+    @FXML
+    public JFXButton homeBtn, watchListBtn, aboutBtn, logOutBtn;
+    @FXML
+    public Label userNameLabel;
+    @FXML
+    public BorderPane mainViewContent;
+    @FXML
+    public AnchorPane contentView;
 
-    public void clickHomeBtn(ActionEvent actionEvent) {
+    public void clickHomeBtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/at/ac/fhcampuswien/fhmdb/content/home.fxml"));
+        contentView = fxmlLoader.load();
     }
 
-    public void clickWatchListBtn(ActionEvent actionEvent) {
+    public void clickWatchListBtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/at/ac/fhcampuswien/fhmdb/content/watchlist.fxml"));
+        AnchorPane root = fxmlLoader.load();
+        mainViewContent.setCenter(root);
     }
 
     public void clickAboutBtn(ActionEvent actionEvent) {
@@ -23,7 +48,12 @@ public class MainViewController implements Initializable {
 
     }
 
-    public void clickLogOutBtn(ActionEvent actionEvent) {
+    public void clickLogOutBtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/at/ac/fhcampuswien/fhmdb/content/loginview.fxml"));
+       AnchorPane view = fxmlLoader.load();
+
+        //contentView = fxmlLoader.load();
+        mainViewContent.setCenter(view);
     }
 
     /*@Override
