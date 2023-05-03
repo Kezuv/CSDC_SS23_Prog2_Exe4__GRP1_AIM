8e720dbc0cfdb20bed9bf85a36a7fe00ebe89a23
kezuv <armin.foucek@gmail.com>
2023-05-03 20:40:19 +0200

    Bugfix with styles Overworked Login-Page

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/controller/LoginViewController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/controller/LoginViewController.java
@@ -1,4 +1,11 @@
 package at.ac.fhcampuswien.fhmdb.ui.controller;
 
+import javafx.event.ActionEvent;
+
 public class LoginViewController {
+    public void clickLoginBtn(ActionEvent actionEvent) {
+    }
+
+    public void clickRegisterBtn(ActionEvent actionEvent) {
+    }
 }
