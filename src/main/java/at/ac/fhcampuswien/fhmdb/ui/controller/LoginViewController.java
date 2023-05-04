4671e51c590bf124d5ad32ee7aa4a0e4d65f8cd4
ibrakel <113463426+ibrakel@users.noreply.github.com>
2023-05-04 17:42:29 +0200

    add init for loginView

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/controller/LoginViewController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/controller/LoginViewController.java
@@ -1,4 +1,32 @@
 package at.ac.fhcampuswien.fhmdb.ui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginViewController {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    public void initialize() {
+        // Set up functionality for login and register buttons
+        loginButton.setOnAction(event -> {
+            // Add code to handle login button click
+        });
+
+        registerButton.setOnAction(event -> {
+            // Add code to handle register button click
+        });
+    }
 }
