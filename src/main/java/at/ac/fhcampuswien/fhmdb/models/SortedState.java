a95efd8604b6f62f1cf0a0f6bdc639cc901d5c88
ibrakel <113463426+ibrakel@users.noreply.github.com>
2023-05-12 16:26:05 +0200

    remove unnecessary code

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/SortedState.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/SortedState.java
@@ -1,21 +1,8 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
-import at.ac.fhcampuswien.fhmdb.Exceptions.MovieExceptions;
-
 public enum SortedState {
     ASCENDING,
     DESCENDING,
     NONE;
-    public static SortedState fromString(String s) throws MovieExceptions.InvalidSortedStateException {
-        switch (s.toLowerCase()) {
-            case "ascending":
-                return ASCENDING;
-            case "descending":
-                return DESCENDING;
-            case "none":
-                return NONE;
-            default:
-                throw new MovieExceptions.InvalidSortedStateException("Invalid sorted state: " + s);
-        }
-    }
+
 }
