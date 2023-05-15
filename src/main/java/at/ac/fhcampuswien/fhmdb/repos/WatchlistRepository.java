f09f949e685757189553422fc7c2eb3c47e12fe5
ibrakel <113463426+ibrakel@users.noreply.github.com>
2023-05-15 18:13:51 +0200

    refactor getAllMoviesForUser

--- src/main/java/at/ac/fhcampuswien/fhmdb/repos/WatchlistRepository.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/repos/WatchlistRepository.java
@@ -26,24 +26,21 @@ public class WatchlistRepository {
 
     //Returns all movies in the watchlist for a user
     public static List<Movie> getAllMoviesForUser(User user) throws DatabaseException.GetAllMoviesException, SQLException {
-    try {
-        List<Movie> movies = new ArrayList<>();
+        try {
+            List<Movie> movies = new ArrayList<>();
 
-        QueryBuilder<WatchlistMovieEntity, Long> queryBuilder = watchlistDao.queryBuilder();
-        queryBuilder.where().eq("USER", user.getId());
+            QueryBuilder<WatchlistMovieEntity, Long> queryBuilder = watchlistDao.queryBuilder();
+            queryBuilder.where().eq("USER", user.getId());
 
-        List<WatchlistMovieEntity> watchlist = queryBuilder.query();
+            List<WatchlistMovieEntity> watchlist = queryBuilder.query();
 
-        for (WatchlistMovieEntity watchlistMovie : watchlist) {
-            movies.add(MovieRepository.movieEntityToMovie(watchlistMovie.getMovie()));
+            for (WatchlistMovieEntity watchlistMovie : watchlist) {
+                movies.add(MovieRepository.movieEntityToMovie(watchlistMovie.getMovie()));
+            }
+            return movies;
+        } catch (SQLException | DatabaseException.GetMovieException e){
+            throw new DatabaseException.GetMoviesForUserException("Failed to retrieve watchlist movies for user: " + user.getUsername(), e);
         }
-        return movies;
-    } catch (SQLException e){
-        throw new DatabaseException.GetMoviesForUserException("Failed to retrieve watchlist movies for user: " + user.getUsername(), e);
-    } catch (DatabaseException.GetMovieException gme){
-
-    }
-    return null;
     }
 
     public static void addMovieToWatchList(User activeUser, Movie movieToAdd) throws DatabaseException.AddMovieToWatchlistException {
